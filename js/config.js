9eb6f836f21ca4197cc1c69ad06c0dd308e22ff1
Aldo Valdez <valdezinsfran@gmail.com>
2026-03-09 02:23:33 -0300

    release: FlowPOS v3.0.0

--- js/config.js
+++ js/config.js
@@ -14,8 +14,8 @@ window.FLOWPOS_CONFIG = {
   // CONFIGURACIÓN PRINCIPAL - Solo cambiar aquí para nuevas versiones
   // ============================================================================
 
-  version: '2.4.10',
-  releaseDate: '2026-03-02',
+  version: '3.0.0',
+  releaseDate: '2026-03-09',
 
   // ============================================================================
   // REPOSITORIO
